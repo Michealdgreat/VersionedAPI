6583e0b8482def429eda457b1922261a32207e85
MICHEAL SHODAMOLA <micheal.shodamola@centria.fi>
2023-12-18 00:56:50 +0200

    Add project files.

---
+++ VersionedAPI/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VersionedAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersionNeutral]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("ping")]
+
+        public IActionResult Ping()
+        {
+            return Ok( "Everything seems great");
+        }
+    }
+}
---
+++ VersionedAPI/Program.cs
@@ -0,0 +1,88 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(opts =>
+{
+    var title = "Our versioned API";
+    var description = "This is a web API that demostrate versioning";
+    var terms = new Uri("https://localhost:7075/terms");
+    var license = new OpenApiLicense()
+    {
+        Name = "This is my full license information or link to it.",
+    };
+
+    var contact = new OpenApiContact()
+    {
+        Name = "This is my full license information or link to it.",
+        Email = "Help@michealshodamolaAPI.com",
+        Url = new Uri("https://michealshodamola.com")
+    };
+
+  
+    opts.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Version = "v2",
+        Title = $"{title} v2",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+
+    });
+
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = $"{title} v1",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+
+    });
+
+});
+builder.Services.AddApiVersioning(opts =>
+{
+    opts.AssumeDefaultVersionWhenUnspecified = true;
+    opts.DefaultApiVersion = new(1, 0);
+    opts.ReportApiVersions = true;
+});
+
+builder.Services.AddVersionedApiExplorer(opts =>
+{
+    opts.GroupNameFormat = "'v'VVV";
+    opts.SubstituteApiVersionInUrl = true;
+});
+
+builder.Services.AddVersionedApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(opts =>
+    {
+        opts.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
